88c866cd3188d725a0f4ae1dbeda720e01a59422
George Zacharia <george.zcharia@gmail.com>
2023-06-11 02:11:50 +0530

    models: Wire SensorSystem with EnvironmentalReading

--- lib/models/envt_reading/envt_reading.dart
+++ lib/models/envt_reading/envt_reading.dart
@@ -1,14 +1,18 @@
+import '../sensor_system/sensor_system.dart';
+
 class EnvironmentalReading {
   String? id;
   double? temperature;
   double? humidity;
   String? timestamp;
+  SensorSystem? sensorSystem;
 
   EnvironmentalReading({
     this.id,
     this.temperature,
     this.humidity,
     this.timestamp,
+    this.sensorSystem,
   });
 
   EnvironmentalReading.fromJson(Map<String, dynamic> json) {
@@ -16,6 +20,9 @@ class EnvironmentalReading {
     temperature = json['temperature'];
     humidity = json['humidity'];
     timestamp = json['timestamp'];
+    sensorSystem = json['sensorSystem'] != null
+        ? SensorSystem.fromJson(json['sensorSystem'])
+        : null;
   }
 
   Map<String, dynamic> toJson() {
@@ -24,6 +31,9 @@ class EnvironmentalReading {
     data['temperature'] = temperature;
     data['humidity'] = humidity;
     data['timestamp'] = timestamp;
+    if (sensorSystem != null) {
+      data['sensorSystem'] = sensorSystem!.toJson();
+    }
     return data;
   }
 }
