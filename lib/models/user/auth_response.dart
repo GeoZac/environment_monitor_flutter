00254d89ba68fc6026d98ff9ac2f16b4470be6bc
George Zacharia <george.zcharia@gmail.com>
2023-07-05 07:45:57 +0530

    lib: models: user: auth_response: Rework making fields non-nullable
    These attributes are always present on entity

--- lib/models/user/auth_response.dart
+++ lib/models/user/auth_response.dart
@@ -1,30 +1,28 @@
 import 'unconv_user.dart';
 
 class AuthResponse {
-  int? expires;
-  UnconvUser? unconvUser;
-  String? token;
+  int expires;
+  UnconvUser unconvUser;
+  String token;
 
   AuthResponse({
-    this.expires,
-    this.unconvUser,
-    this.token,
+    required this.expires,
+    required this.unconvUser,
+    required this.token,
   });
 
-  AuthResponse.fromJson(Map<String, dynamic> json) {
-    expires = json['expires'];
-    unconvUser = json['unconvUser'] != null
-        ? UnconvUser.fromJson(json['unconvUser'])
-        : null;
-    token = json['token'];
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      expires: json['expires'],
+      unconvUser: UnconvUser.fromJson(json['unconvUser']),
+      token: json['token'],
+    );
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['expires'] = expires;
-    if (unconvUser != null) {
-      data['unconvUser'] = unconvUser!.toJson();
-    }
+    data['unconvUser'] = unconvUser.toJson();
     data['token'] = token;
     return data;
   }
