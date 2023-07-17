e53e2cac48c3e32b34c48e7caa91b12dff88f4d2
George Zacharia <george.zcharia@gmail.com>
2023-07-17 11:00:26 +0530

    test: models: sensor_system: sensor_system_test: Add test for `toJson`

--- test/models/sensor_system/sensor_system_test.dart
+++ test/models/sensor_system/sensor_system_test.dart
@@ -1,4 +1,5 @@
 import 'package:environment_monitor/models/sensor_system/sensor_system.dart';
+import 'package:environment_monitor/models/user/unconv_user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -40,4 +41,25 @@ void main() {
 
     expect(() => SensorSystem.fromJson(json2), throwsFormatException);
   }));
+
+  test('toJson() should return a valid JSON map', () {
+    // Create an instance of the class to test
+    final instance = SensorSystem(
+      id: "508baef0-bc82-4481-9af8-83d2e5132100",
+      sensorName: "Test Sensor",
+      unconvUser: UnconvUser(
+        id: "a5bbd1bd-c89b-4219-b0a8-379abe41b879",
+        username: "Test User",
+        email: "test@example.com",
+      ),
+    );
+
+    final json = instance.toJson();
+
+    expect(json, isA<Map<String, dynamic>>());
+
+    expect(json['id'], equals('508baef0-bc82-4481-9af8-83d2e5132100'));
+    expect(json['sensorName'], equals('Test Sensor'));
+    expect(json['unconvUser'], isA<Map<String, dynamic>>());
+  });
 }
