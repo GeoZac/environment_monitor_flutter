ef797d6d0526a18b053997b18bd8ce6b63692612
George Zacharia <george.zcharia@gmail.com>
2023-07-17 10:59:29 +0530

    test: models: envt_reading: envt_reading_test: Add test for  `toJson()`

--- test/models/envt_reading/envt_reading_test.dart
+++ test/models/envt_reading/envt_reading_test.dart
@@ -1,4 +1,6 @@
 import 'package:environment_monitor/models/envt_reading/envt_reading.dart';
+import 'package:environment_monitor/models/sensor_system/sensor_system.dart';
+import 'package:environment_monitor/models/user/unconv_user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -57,4 +59,33 @@ void main() {
 
     expect(() => EnvironmentalReading.fromJson(json2), throwsFormatException);
   }));
+
+  test('toJson() should return a valid JSON map', () {
+    // Create an instance of the class to test
+    final instance = EnvironmentalReading(
+      id: "0002d2ce-6441-4975-bdb9-a55667ebfb23",
+      temperature: 25.0,
+      humidity: 50.0,
+      timestamp: "2023-06-21T22:10:29Z",
+      sensorSystem: SensorSystem(
+        id: "508baef0-bc82-4481-9af8-83d2e5132100",
+        sensorName: "Test Sensor",
+        unconvUser: UnconvUser(
+          id: "a5bbd1bd-c89b-4219-b0a8-379abe41b879",
+          username: "Test User",
+          email: "test@example.com",
+        ),
+      ),
+    );
+
+    final json = instance.toJson();
+
+    expect(json, isA<Map<String, dynamic>>());
+
+    expect(json['id'], equals('0002d2ce-6441-4975-bdb9-a55667ebfb23'));
+    expect(json['temperature'], equals(25.0));
+    expect(json['humidity'], equals(50.0));
+    expect(json['timestamp'], isNotNull);
+    expect(json['sensorSystem'], isA<Map<String, dynamic>>());
+  });
 }
