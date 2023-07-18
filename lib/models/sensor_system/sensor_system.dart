a63e56fb1ab06c3f06ba4be56af5f8ac4e4fedef
George Zacharia <george.zcharia@gmail.com>
2023-07-18 08:03:54 +0530

    lib: models: sensor_system: sensor_system: Rework for nullability

--- lib/models/sensor_system/sensor_system.dart
+++ lib/models/sensor_system/sensor_system.dart
@@ -1,43 +1,40 @@
 import '../user/unconv_user.dart';
 
 class SensorSystem {
-  String? id;
-  String? sensorName;
-  UnconvUser? unconvUser;
+  String id;
+  String sensorName;
+  UnconvUser unconvUser;
 
   SensorSystem({
-    this.id,
-    this.sensorName,
-    this.unconvUser,
+    required this.id,
+    required this.sensorName,
+    required this.unconvUser,
   });
 
-  SensorSystem.fromJson(Map<String, dynamic> json) {
+  factory SensorSystem.fromJson(Map<String, dynamic> json) {
     final List<String> requiredFields = [
       'id',
       'sensorName',
       'unconvUser',
     ];
 
-    final bool missingFields =
-        requiredFields.any((field) => json[field] == null);
+    final bool missingFields = requiredFields.any((field) => json[field] == null);
     if (missingFields) {
       throw const FormatException("Missing required fields in JSON");
     }
 
-    id = json['id'];
-    sensorName = json['sensorName'];
-    unconvUser = json['unconvUser'] != null
-        ? UnconvUser.fromJson(json['unconvUser'])
-        : null;
+    return SensorSystem(
+      id: json['id'],
+      sensorName: json['sensorName'],
+      unconvUser: UnconvUser.fromJson(json['unconvUser']),
+    );
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['id'] = id;
     data['sensorName'] = sensorName;
-    if (unconvUser != null) {
-      data['unconvUser'] = unconvUser!.toJson();
-    }
+    data['unconvUser'] = unconvUser.toJson();
     return data;
   }
 }
