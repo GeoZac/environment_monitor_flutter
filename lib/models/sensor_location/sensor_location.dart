abc6f07017d1b9a7ee5d05bae180827b2722244f
George Zacharia <george.zcharia@gmail.com>
2023-07-18 23:28:22 +0530

    lib: models: sensor_location: sensor_location: Add class to hold Sensor Location

---
+++ lib/models/sensor_location/sensor_location.dart
@@ -0,0 +1,74 @@
+import '../../consts/sensor_location_type.dart';
+
+class SensorLocation {
+  String id;
+  String sensorLocationText;
+  double latitude;
+  double longitude;
+  SensorLocationType sensorLocationType;
+
+  SensorLocation({
+    required this.id,
+    required this.sensorLocationText,
+    required this.latitude,
+    required this.longitude,
+    required this.sensorLocationType,
+  });
+
+  factory SensorLocation.fromJson(Map<String, dynamic> json) {
+    final List<String> requiredFields = [
+      'id',
+      'sensorLocationText',
+      'latitude',
+      'longitude',
+      'sensorLocationType',
+    ];
+
+    final bool missingFields =
+        requiredFields.any((field) => json[field] == null);
+    if (missingFields) {
+      throw const FormatException("Missing required fields in JSON");
+    }
+
+    return SensorLocation(
+      id: json['id'],
+      sensorLocationText: json['sensorLocationText'],
+      latitude: json['latitude'],
+      longitude: json['longitude'],
+      sensorLocationType:
+          parseSensorLocationTypeFromJson(json['sensorLocationType']),
+    );
+  }
+
+  static SensorLocationType parseSensorLocationTypeFromJson(String value) {
+    switch (value) {
+      case 'INDOOR':
+        return SensorLocationType.indoor;
+      case 'OUTDOOR':
+        return SensorLocationType.outdoor;
+      default:
+        throw ArgumentError('Invalid sensor location type: $value');
+    }
+  }
+
+  static String parseSensorLocationTypeToJson(SensorLocationType value) {
+    switch (value) {
+      case SensorLocationType.indoor:
+        return 'INDOOR';
+      case SensorLocationType.outdoor:
+        return 'OUTDOOR';
+      default:
+        throw ArgumentError('Invalid sensor location type: $value');
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'sensorLocationText': sensorLocationText,
+      'latitude': latitude,
+      'longitude': longitude,
+      'sensorLocationType': parseSensorLocationTypeToJson(sensorLocationType),
+    };
+  }
+}
