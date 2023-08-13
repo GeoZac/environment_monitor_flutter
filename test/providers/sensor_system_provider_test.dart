59ff8ba9adccd96691dcd339d41fde0ea79cb9b5
George Zacharia <george.zcharia@gmail.com>
2023-08-13 11:42:03 +0530

    test: providers: sensor_system_provider_test: Generate Mock class

---
+++ test/providers/sensor_system_provider_test.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:environment_monitor/models/sensor_system/sensor_system_response.dart';
+import 'package:environment_monitor/providers/sensor_system_provider.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'sensor_system_provider_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  // Placeholder method to generate the mock
+}
