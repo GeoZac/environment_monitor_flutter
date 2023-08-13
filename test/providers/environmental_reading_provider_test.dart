49291b40ac5edb4f33c17c1dfeb4bb6fb235c09e
George Zacharia <george.zcharia@gmail.com>
2023-08-13 12:20:38 +0530

    test: providers: environmental_reading_provider_test: Generate mock class

---
+++ test/providers/environmental_reading_provider_test.dart
@@ -0,0 +1,7 @@
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  // Placeholder method to get Mock class generated
+}
