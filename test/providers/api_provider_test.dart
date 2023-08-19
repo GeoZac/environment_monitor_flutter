2f444a576a9f6ac9e2dd6387d0efc4bae633c393
George Zacharia <george.zcharia@gmail.com>
2023-08-19 22:58:12 +0530

    test: providers: api_provider_test: Add a test for 400 Status code

--- test/providers/api_provider_test.dart
+++ test/providers/api_provider_test.dart
@@ -112,4 +112,36 @@ void main() {
       );
     });
   });
+
+  test('Test makeHttpPost 400 failure', () {
+    final mockClient = MockClient();
+    ApiProvider apiProvider = ApiProvider(mockClient);
+
+    final url = Uri.parse('https://example.com/api/invalid_endpoint');
+    final requestBody = {'key': 'value'};
+
+    when(
+      mockClient.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(requestBody),
+      ),
+    ).thenAnswer(
+      (_) async => http.Response(
+        '{"key": "value"}',
+        400,
+      ),
+    );
+
+    expect(
+      () async => await apiProvider.makeHttpPost(
+        url,
+        null,
+        requestBody,
+      ),
+      throwsA(
+        isA<BadRequestException>(),
+      ),
+    );
+  });
 }
