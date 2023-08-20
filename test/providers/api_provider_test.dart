0c7daa79e9a4e582e094d6532927d4e5b4c5db5c
George Zacharia <george.zcharia@gmail.com>
2023-08-20 06:20:30 +0530

    test: providers: api_provider_test: Add a test for 500 Status code

--- test/providers/api_provider_test.dart
+++ test/providers/api_provider_test.dart
@@ -208,4 +208,36 @@ void main() {
       ),
     );
   });
+
+  test('Test makeHttpPost 500 failure', () {
+    final mockClient = MockClient();
+    ApiProvider apiProvider = ApiProvider(mockClient);
+
+    final url = Uri.parse('https://example.com/api/invalid_endpoint');
+    final requestBody = {'key': 'value'};
+
+    when(
+      mockClient.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(requestBody),
+      ),
+    ).thenAnswer(
+      (_) async => http.Response(
+        '{"key": "value"}',
+        500,
+      ),
+    );
+
+    expect(
+      () async => await apiProvider.makeHttpPost(
+        url,
+        null,
+        requestBody,
+      ),
+      throwsA(
+        isA<FetchDataException>(),
+      ),
+    );
+  });
 }
