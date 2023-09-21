ed12ce463077afeb317f3d234204f29f8f7e1066
George Zacharia <george.zcharia@gmail.com>
2023-09-22 05:24:34 +0530

    lib: providers: sensor_system_provider: Add method to create new SensorSystem
    Allows to register new sensor systems from app

--- lib/providers/sensor_system_provider.dart
+++ lib/providers/sensor_system_provider.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import '../config/secrets.dart';
+import '../models/sensor_system/sensor_system.dart';
 import '../models/sensor_system/sensor_system_response.dart';
 import 'api_provider.dart';
 
 class SensorSystemProvider with ChangeNotifier {
   final http.Client httpClient;
+
+  final String baseUrlPath = "/SensorSystem";
 
   SensorSystemProvider(
     this.httpClient,
@@ -21,7 +24,7 @@ class SensorSystemProvider with ChangeNotifier {
     Uri uri = Uri(
       scheme: 'https',
       host: Secrets.baseApiUrl,
-      path: '/SensorSystem/UnconvUser/$unconvUserId',
+      path: '$baseUrlPath/UnconvUser/$unconvUserId',
     );
 
     ApiProvider apiProvider = ApiProvider(
@@ -33,5 +36,28 @@ class SensorSystemProvider with ChangeNotifier {
     );
 
     return SensorSystemResponse.fromJson(response);
+  }
+
+  Future<SensorSystem> createSensorSystem(SensorSystem sensorSystem) async {
+    Map<String, String> headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer ${Secrets.bearerToken}",
+    };
+
+    Uri uri = Uri(
+      scheme: 'https',
+      host: Secrets.baseApiUrl,
+      path: baseUrlPath,
+    );
+
+    ApiProvider apiProvider = ApiProvider(
+      httpClient,
+    );
+    final response = await apiProvider.makeHttpPost(
+      uri,
+      headers,
+      sensorSystem.toJson(),
+    );
+    return SensorSystem.fromJson(response['entity']);
   }
 }
