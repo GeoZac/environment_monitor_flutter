77898e8eefea6bed808f854cf57069f2279b6ae1
George Zacharia <george.zcharia@gmail.com>
2024-01-15 07:45:48 +0530

    lib: consts: Bring in some color constants

---
+++ lib/consts/color_consts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color appBarBackground = Color(0xffefeee6);
+  static const Color backgroundColor = Color(0xfffbf9f1);
+  static const Color borderOutlineColor = Color(0xff4c6707);
+  static const Color textColorGreen = Color(0xff4c6707);
+  static const Color textColorBlack = Color(0xff1b1c17);
+  static const Color textColorMuted = Color(0xff787878);
+  static const Color mutedIconColor = Color.fromARGB(73, 69, 79, 1);
+  static const Color buttonSurface = Color(0xffcdef84);
+  static const Color errorColor = Color(0xffba1a1a);
+}
