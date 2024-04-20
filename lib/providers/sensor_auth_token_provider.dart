0049208e131c33948c1b0bfad6533000ca11a31e
George Zacharia <george.zcharia@gmail.com>
2024-04-20 15:50:46 +0530

    lib: providers: sensor_auth_token_provider: Add method to generate SensorAuthToken

--- lib/providers/sensor_auth_token_provider.dart
+++ lib/providers/sensor_auth_token_provider.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 
 import '../config/secrets.dart';
 import '../models/sensor_auth_token/sensor_auth_token.dart';
+import '../models/shared/message_response.dart';
 import 'api_provider.dart';
 
 class SensorAuthTokenProvider with ChangeNotifier {
@@ -38,5 +39,30 @@ class SensorAuthTokenProvider with ChangeNotifier {
       return null;
     }
     return SensorAuthToken.fromJson(response);
+  }
+
+  Future<MessageResponse<SensorAuthToken>> generateSensorAuthToken(
+      String sensorSystemId) async {
+    Map<String, String> headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer ${Secrets.bearerToken}",
+    };
+
+    Uri uri = Uri(
+      scheme: 'https',
+      host: Secrets.baseApiUrl,
+      path: '$baseUrlPath/GenerateToken/SensorSystem/$sensorSystemId',
+    );
+
+    ApiProvider apiProvider = ApiProvider(
+      httpClient,
+    );
+    final response = await apiProvider.makeHttpGet(
+      uri,
+      headers: headers,
+    );
+
+    return MessageResponse.fromJson(
+        response, (p0) => SensorAuthToken.fromJson(p0));
   }
 }
