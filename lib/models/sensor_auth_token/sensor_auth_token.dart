c28264d7c179797c26ef4fb83403ce388c39d6e6
George Zacharia <george.zcharia@gmail.com>
2024-04-24 21:32:04 +0530

    lib: models: sensor_auth_token: sensor_auth_token: Add docs

--- lib/models/sensor_auth_token/sensor_auth_token.dart
+++ lib/models/sensor_auth_token/sensor_auth_token.dart
@@ -1,11 +1,26 @@
 import '../sensor_system/sensor_system.dart';
 
+/// A class representing an authentication token for a sensor system.
+///
+/// This class holds information about an authentication token used for accessing
+/// a sensor system, including its unique identifier, authentication token,
+/// expiry date, and the associated [SensorSystem].
 class SensorAuthToken {
+  /// The unique identifier of the authentication token.
   String id;
+
+  /// The authentication token string.
   String authToken;
+
+  /// The expiry date of the authentication token.
   String expiry;
+
+  /// The sensor system associated with this authentication token.
   SensorSystem sensorSystem;
 
+  /// Constructs a [SensorAuthToken] instance with the provided parameters.
+  ///
+  /// All parameters are required.
   SensorAuthToken({
     required this.id,
     required this.authToken,
@@ -13,6 +28,13 @@ class SensorAuthToken {
     required this.sensorSystem,
   });
 
+  /// Constructs a [SensorAuthToken] instance from a JSON map.
+  ///
+  /// The [json] parameter should be a map representing the JSON structure
+  /// of the authentication token.
+  ///
+  /// Throws a [FormatException] if any of the required fields are missing
+  /// in the JSON.
   factory SensorAuthToken.fromJson(Map<String, dynamic> json) {
     final List<String> requiredFields = [
       'id',
