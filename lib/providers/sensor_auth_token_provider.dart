4dcc9f1d7e39f10e3591e9181da3c804def01ba8
George Zacharia <george.zcharia@gmail.com>
2024-04-24 21:38:42 +0530

    lib: providers: sensor_auth_token_provider: Add docs

--- lib/providers/sensor_auth_token_provider.dart
+++ lib/providers/sensor_auth_token_provider.dart
@@ -6,15 +6,28 @@ import '../models/sensor_auth_token/sensor_auth_token.dart';
 import '../models/shared/message_response.dart';
 import 'api_provider.dart';
 
+/// A class responsible for providing functionality related to sensor authentication tokens.
+///
+/// This class facilitates fetching and generating sensor authentication tokens
+/// using HTTP requests to the API endpoints.
 class SensorAuthTokenProvider with ChangeNotifier {
+  /// The HTTP client used for making API requests.
   final http.Client httpClient;
 
+  /// The base path for sensor authentication token API endpoints.
   final String baseUrlPath = "/SensorAuthToken";
 
+  /// Constructs a [SensorAuthTokenProvider] instance with the provided HTTP client.
+  ///
+  /// The [httpClient] parameter is required.
   SensorAuthTokenProvider(
     this.httpClient,
   );
 
+  /// Fetches a sensor authentication token for the specified sensor system.
+  ///
+  /// Returns a [Future] that resolves to a [SensorAuthToken] instance if successful,
+  /// otherwise returns `null`.
   Future<SensorAuthToken?> fetchSensorAuthToken(String sensorSystemId) async {
     Map<String, String> headers = {
       "Content-Type": "application/json",
@@ -41,6 +54,10 @@ class SensorAuthTokenProvider with ChangeNotifier {
     return SensorAuthToken.fromJson(response);
   }
 
+  /// Generates a sensor authentication token for the specified sensor system.
+  ///
+  /// Returns a [Future] that resolves to a [MessageResponse] instance containing
+  /// the generated authentication token if successful.
   Future<MessageResponse<SensorAuthToken>> generateSensorAuthToken(
       String sensorSystemId) async {
     Map<String, String> headers = {
