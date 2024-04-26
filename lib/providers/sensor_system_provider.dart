0698143323beba99c6f5282cba06ca3eb71d4361
George Zacharia <george.zcharia@gmail.com>
2024-04-26 08:17:32 +0530

    lib: providers: sensor_system_provider: Add a method to fetch recent readings

--- lib/providers/sensor_system_provider.dart
+++ lib/providers/sensor_system_provider.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import '../config/secrets.dart';
+import '../models/misc/recent_readings.dart';
 import '../models/sensor_system/sensor_system_response.dart';
 import 'api_provider.dart';
 
 class SensorSystemProvider with ChangeNotifier {
   final http.Client httpClient;
+
+  final String baseUrlPath = "/SensorSystem";
 
   SensorSystemProvider(
     this.httpClient,
@@ -33,5 +36,29 @@ class SensorSystemProvider with ChangeNotifier {
     );
 
     return SensorSystemResponse.fromJson(response);
+  }
+
+  Future<RecentReadings> fetchRecentReadings(String sensorSystemId) async {
+    Map<String, String> headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer ${Secrets.bearerToken}",
+    };
+
+    Uri uri = Uri(
+      scheme: 'https',
+      host: Secrets.baseApiUrl,
+      path: '$baseUrlPath/ReadingsCount/$sensorSystemId',
+    );
+
+    ApiProvider apiProvider = ApiProvider(
+      httpClient,
+    );
+    final response = await apiProvider.makeHttpGet(
+      uri,
+      headers: headers,
+    );
+    return RecentReadings.fromJson(
+      response,
+    );
   }
 }
