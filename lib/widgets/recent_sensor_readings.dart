1ed1de8816c63dd3fe53d4cdceec4482463a0285
George Zacharia <george.zcharia@gmail.com>
2025-01-26 07:50:09 +0530

    lib: widgets: recent_sensor_readings: Init

---
+++ lib/widgets/recent_sensor_readings.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/misc/recent_readings.dart';
+import '../providers/sensor_system_provider.dart';
+import 'recent_readings_row.dart';
+
+class RecentReadingsCard extends StatefulWidget {
+  final String sensorSystemId;
+
+  RecentReadingsCard({
+    super.key,
+    required this.sensorSystemId,
+  });
+
+  @override
+  State<RecentReadingsCard> createState() => _RecentReadingsCardState();
+}
+
+class _RecentReadingsCardState extends State<RecentReadingsCard> {
+  bool _init = false;
+  bool isViewExpanded = false;
+
+  late RecentReadings recentReadings;
+  // final sensorSystemProvider = getIt<SensorSystemProvider>();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchRecentSensorReadings();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return !_init
+        ? Container()
+        : Card(
+            child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Recent reading count",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: isViewExpanded ? 20 : 24,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      expandReadingInfo();
+                    },
+                    icon: Icon(
+                        isViewExpanded ? Icons.expand_less : Icons.expand_more),
+                  )
+                ],
+              ),
+              if (isViewExpanded)
+                RecentReadingsRow(
+                  recentReadings: recentReadings,
+                ),
+            ]),
+          ));
+  }
+
+  void fetchRecentSensorReadings() {
+    SensorSystemProvider sensorSystemProvider = SensorSystemProvider(
+      http.Client(),
+    );
+
+    _init = false;
+    sensorSystemProvider
+        .fetchRecentReadings(widget.sensorSystemId)
+        .then((value) {
+      setState(() {
+        recentReadings = value;
+        _init = true;
+      });
+    }).onError((error, stackTrace) {
+      print("HERE");
+      print(error.toString());
+    });
+  }
+
+  void expandReadingInfo() {
+    setState(() {
+      isViewExpanded = !isViewExpanded;
+    });
+  }
+}
