d94bf9e8d258876eb1cfb381bd69773e3313de69
George Zacharia <george.zcharia@gmail.com>
2025-02-10 21:42:58 +0530

    lib: screens: sensor_system_form: Fetch exisitng sensor locations

--- lib/screens/sensor_system_form.dart
+++ lib/screens/sensor_system_form.dart
@@ -2,9 +2,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/sensor_location/sensor_location.dart';
 import '../models/user/unconv_user.dart';
+import '../providers/sensor_location_provider.dart';
+import '../widgets/common/center_circular_progress.dart';
 
 class SensorSystemForm extends StatefulWidget {
   static const routeName = '/sensorSystemForm';
@@ -25,9 +28,13 @@ class _SensorSystemFormState extends State<SensorSystemForm> {
 
   final _formKey = GlobalKey<FormState>();
 
+  late List<SensorLocation> existingSensorLocations;
+
   @override
   void initState() {
     super.initState();
+
+    fetchSensorLocations();
   }
 
   @override
@@ -36,119 +43,139 @@ class _SensorSystemFormState extends State<SensorSystemForm> {
         appBar: AppBar(
           title: const Text('Add New Sensor Form'),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SingleChildScrollView(
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        FormSectionTitle(
-                          titleString: "Device details *",
-                        ),
-                        TextFormField(
-                          autofocus: true,
-                          decoration: InputDecoration(
-                            labelText: 'Sensor Name *',
-                            border: outlineInputBorder8(),
-                          ),
-                          validator: (String? newValue) {
-                            if (newValue == null || newValue.isEmpty) {
-                              return 'Please enter a sensor name';
-                            }
-                            return null;
-                          },
-                          onSaved: null,
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        TextFormField(
-                          maxLength: 500,
-                          maxLines: 3,
-                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                          decoration: InputDecoration(
-                            alignLabelWithHint: true,
-                            labelText: 'Description (Optional)',
-                            border: outlineInputBorder8(),
-                          ),
-                          onSaved: null,
-                          textAlign: TextAlign.start,
-                          textAlignVertical: TextAlignVertical.top,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 8.0,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
+        body: (!_init)
+            ? const CenteredCircularProgress()
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SingleChildScrollView(
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               FormSectionTitle(
-                                titleString: "Location details *",
+                                titleString: "Device details *",
                               ),
-                              TextButton(
-                                  onPressed: () {},
-                                  child: Text(
-                                    "Add new",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
+                              TextFormField(
+                                autofocus: true,
+                                decoration: InputDecoration(
+                                  labelText: 'Sensor Name *',
+                                  border: outlineInputBorder8(),
+                                ),
+                                validator: (String? newValue) {
+                                  if (newValue == null || newValue.isEmpty) {
+                                    return 'Please enter a sensor name';
+                                  }
+                                  return null;
+                                },
+                                onSaved: null,
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              TextFormField(
+                                maxLength: 500,
+                                maxLines: 3,
+                                maxLengthEnforcement:
+                                    MaxLengthEnforcement.enforced,
+                                decoration: InputDecoration(
+                                  alignLabelWithHint: true,
+                                  labelText: 'Description (Optional)',
+                                  border: outlineInputBorder8(),
+                                ),
+                                onSaved: null,
+                                textAlign: TextAlign.start,
+                                textAlignVertical: TextAlignVertical.top,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 8.0,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    FormSectionTitle(
+                                      titleString: "Location details *",
                                     ),
-                                  ))
+                                    TextButton(
+                                        onPressed: () {},
+                                        child: Text(
+                                          "Add new",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ))
+                                  ],
+                                ),
+                              ),
+                              DropdownMenu<SensorLocation>(
+                                hintText: "Select a Sensor Location",
+                                enableSearch: true,
+                                inputDecorationTheme: InputDecorationTheme(
+                                  border: outlineInputBorder8(),
+                                ),
+                                dropdownMenuEntries:
+                                    getSensorLocationDropDownEntries(),
+                                onSelected: null,
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
                             ],
                           ),
                         ),
-                        DropdownMenu<SensorLocation>(
-                          hintText: "Select a Sensor Location",
-                          enableSearch: true,
-                          inputDecorationTheme: InputDecorationTheme(
-                            border: outlineInputBorder8(),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          onPressed: null,
+                          child: const Text(
+                            'Submit',
+                            style: TextStyle(fontSize: 24),
                           ),
-                          dropdownMenuEntries:
-                              getSensorLocationDropDownEntries(),
-                          onSelected: null,
                         ),
-                        const SizedBox(
-                          height: 16,
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text(
+                            'Cancel',
+                            style: TextStyle(fontSize: 24),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    onPressed: null,
-                    child: const Text(
-                      'Submit',
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text(
-                      'Cancel',
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  ),
                 ],
-              ),
-            ),
-          ],
-        ));
+              ));
+  }
+
+  void fetchSensorLocations() {
+    _init = false;
+    SensorLocationProvider sensorSystemProvider = SensorLocationProvider(
+      http.Client(),
+    );
+    sensorSystemProvider
+        .fetchSensorLocations(widget.unconvUser.id)
+        .then((response) {
+      existingSensorLocations = response;
+
+      setState(() {
+        _init = true;
+      });
+    });
   }
 }
 
