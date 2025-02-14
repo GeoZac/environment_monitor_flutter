18446269c8cbeb75f4e43f0d2f3b29b42d155c7b
George Zacharia <george.zcharia@gmail.com>
2025-02-14 22:44:40 +0530

    test: models: envt_reading: envt_reading_test: Update for 7259b34

--- test/models/envt_reading/envt_reading_test.dart
+++ test/models/envt_reading/envt_reading_test.dart
@@ -1,6 +1,6 @@
 import 'package:environment_monitor/consts/sensor_status.dart';
 import 'package:environment_monitor/models/envt_reading/envt_reading.dart';
-import 'package:environment_monitor/models/sensor_system/sensor_system_dto.dart';
+import 'package:environment_monitor/models/sensor_system/sensor_system.dart';
 import 'package:environment_monitor/models/user/unconv_user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -74,7 +74,7 @@ void main() {
       temperature: 25.0,
       humidity: 50.0,
       timestamp: "2023-06-21T22:10:29Z",
-      sensorSystem: SensorSystemDTO(
+      sensorSystem: SensorSystem(
         id: "508baef0-bc82-4481-9af8-83d2e5132100",
         sensorName: "Test Sensor",
         deleted: false,
@@ -84,7 +84,6 @@ void main() {
           username: "Test User",
           email: "test@example.com",
         ),
-        readingCount: 0,
       ),
     );
 
