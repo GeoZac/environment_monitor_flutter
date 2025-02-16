302cad663be3c2d97cb92c9ce957081838c9f4c1
George Zacharia <george.zcharia@gmail.com>
2025-02-16 11:04:23 +0530

    lib: models: threshold: threshold: Add field validations

--- lib/models/threshold/threshold.dart
+++ lib/models/threshold/threshold.dart
@@ -10,6 +10,18 @@ class Threshold {
   });
 
   factory Threshold.fromJson(Map<String, dynamic> json) {
+    final List<String> requiredFields = [
+      'minValue',
+      'maxValue',
+    ];
+    final bool missingFields =
+        requiredFields.any((field) => json[field] == null);
+
+    if (missingFields) {
+      throw const FormatException(
+          "Missing required fields: minValue and maxValue");
+    }
+
     return Threshold(
       id: json['id'],
       minValue: json['minValue'],
