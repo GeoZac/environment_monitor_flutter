01bb347a7f2f34475e0682c6d8d78adb883d9eee
George Zacharia <george.zcharia@gmail.com>
2025-02-16 20:36:28 +0530

    test: models: sensor_system: sensor_system_dto_test: Add new test for thresholds parsing

--- test/models/sensor_system/sensor_system_dto_test.dart
+++ test/models/sensor_system/sensor_system_dto_test.dart
@@ -48,6 +48,84 @@ void main() {
     expect(sensorSystem.unconvUser.email, "noname@email.com");
   });
 
+  test('Test SensorSystem.fromJson with Thresholds', () {
+    // Sample JSON data
+    final json = {
+      "id": "7499e351-f3f7-40f0-aaee-6a5f09070848",
+      "sensorName": "Workspace sensor system",
+      "description": "Monitors temperature and humidity for personal workspace",
+      "deleted": false,
+      "sensorStatus": "ACTIVE",
+      "sensorLocation": {
+        "id": "9c28ce4a-144d-49bd-b303-7e9dae98ac5b",
+        "sensorLocationText": "Parthenon",
+        "latitude": 37.9715,
+        "longitude": 23.7269,
+        "sensorLocationType": "OUTDOOR"
+      },
+      "unconvUser": {
+        "id": "b9b401a5-9509-47c1-b134-200ba288092d",
+        "username": "NewUnconvUser",
+        "email": "newuser@email.com",
+        "authorities": [],
+        "enabled": false,
+        "accountNonExpired": false,
+        "accountNonLocked": false,
+        "credentialsNonExpired": false
+      },
+      "humidityThreshold": {
+        "id": "a60053df-03e8-4f3a-aced-51627e85b8d3",
+        "maxValue": 75.0,
+        "minValue": 23.0
+      },
+      "temperatureThreshold": {
+        "id": "eb32890a-1330-44cd-8199-11e76faa98a9",
+        "maxValue": 100.0,
+        "minValue": 0.0
+      },
+      "readingCount": 4365868485925218167,
+      "latestReading": {
+        "temperature": 32.1,
+        "humidity": 76.5,
+        "timestamp": "2024-07-13T02:13:53.444839418Z"
+      },
+    };
+
+    // Perform the model mapping
+    final SensorSystemDTO sensorSystem = SensorSystemDTO.fromJson(json);
+
+    // Verify the values
+    expect(sensorSystem.id, "7499e351-f3f7-40f0-aaee-6a5f09070848");
+    expect(sensorSystem.sensorName, "Workspace sensor system");
+    expect(sensorSystem.description,
+        "Monitors temperature and humidity for personal workspace");
+    expect(sensorSystem.deleted, false);
+    expect(sensorSystem.sensorStatus, SensorStatus.active);
+    expect(sensorSystem.sensorLocation!.id,
+        "9c28ce4a-144d-49bd-b303-7e9dae98ac5b");
+    expect(sensorSystem.sensorLocation!.sensorLocationText, "Parthenon");
+    expect(sensorSystem.sensorLocation!.latitude, 37.9715);
+    expect(sensorSystem.sensorLocation!.longitude, 23.7269);
+    expect(sensorSystem.sensorLocation!.sensorLocationType,
+        SensorLocationType.outdoor);
+    expect(sensorSystem.unconvUser.id, "b9b401a5-9509-47c1-b134-200ba288092d");
+    expect(sensorSystem.unconvUser.username, "NewUnconvUser");
+    expect(sensorSystem.unconvUser.email, "newuser@email.com");
+    expect(sensorSystem.humidityThreshold!.id!,
+        "a60053df-03e8-4f3a-aced-51627e85b8d3");
+    expect(sensorSystem.humidityThreshold!.maxValue, 75.0);
+    expect(sensorSystem.humidityThreshold!.minValue, 23.0);
+    expect(sensorSystem.temperatureThreshold!.id,
+        "eb32890a-1330-44cd-8199-11e76faa98a9");
+    expect(sensorSystem.temperatureThreshold!.maxValue, 100.0);
+    expect(sensorSystem.temperatureThreshold!.minValue, 0.0);
+    expect(sensorSystem.readingCount, 4365868485925218167);
+    expect(sensorSystem.latestReading!.temperature, 32.1);
+    expect(sensorSystem.latestReading!.humidity, 76.5);
+    expect(sensorSystem.latestReading!.timestamp,
+        "2024-07-13T02:13:53.444839418Z");
+  });
+
   test('Test SensorSystem.fromJson', () {
     // Sample JSON data
     final json = {
