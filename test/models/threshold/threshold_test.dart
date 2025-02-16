51333b824ab2f441a084fc066862ee55f6a7f347
George Zacharia <george.zcharia@gmail.com>
2025-02-16 19:57:14 +0530

    test: models: threshold: threshold_test: Complete test coverage

--- test/models/threshold/threshold_test.dart
+++ test/models/threshold/threshold_test.dart
@@ -16,5 +16,37 @@ void main() {
       expect(threshold.minValue, 10.0);
       expect(threshold.maxValue, 100.0);
     });
+
+    test('fromJson should throw FormatException when minValue is missing', () {
+      final json = {
+        'id': '123',
+        'maxValue': 100.0,
+      };
+
+      expect(() => Threshold.fromJson(json), throwsFormatException);
+    });
+
+    test('fromJson should throw FormatException when maxValue is missing', () {
+      final json = {
+        'id': '123',
+        'minValue': 10.0,
+      };
+
+      expect(() => Threshold.fromJson(json), throwsFormatException);
+    });
+
+    test('toJson should return a valid JSON map', () {
+      final threshold = Threshold(
+        id: '123',
+        minValue: 10.0,
+        maxValue: 100.0,
+      );
+
+      final json = threshold.toJson();
+
+      expect(json['id'], '123');
+      expect(json['minValue'], 10.0);
+      expect(json['maxValue'], 100.0);
+    });
   });
 }
