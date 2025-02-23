3901ea219c6946242255922ff347f012e120f5c4
George Zacharia <george.zcharia@gmail.com>
2025-02-23 11:05:21 +0530

    test: models: exception: bad_request_response_test: Add more tests

--- test/models/exception/bad_request_response_test.dart
+++ test/models/exception/bad_request_response_test.dart
@@ -29,5 +29,52 @@ void main() {
       expect(response.violations![1].message, "Invalid Password");
       expect(response.title, "Constraint Violation");
     });
+
+    test('should convert to JSON correctly', () {
+      final response = BadRequestResponse(
+        type: "https://example.com/probs/out-of-credit",
+        status: 400,
+        violations: [
+          Violations(field: "email", message: "Email is required"),
+          Violations(
+              field: "password",
+              message: "Password must be at least 6 characters long"),
+        ],
+        title: "Bad Request",
+      );
+
+      final jsonResponse = response.toJson();
+
+      expect(jsonResponse['type'], "https://example.com/probs/out-of-credit");
+      expect(jsonResponse['status'], 400);
+      expect(jsonResponse['violations'], hasLength(2));
+      expect(jsonResponse['violations'][0]['field'], "email");
+      expect(jsonResponse['violations'][0]['message'], "Email is required");
+      expect(jsonResponse['violations'][1]['field'], "password");
+      expect(jsonResponse['violations'][1]['message'],
+          "Password must be at least 6 characters long");
+      expect(jsonResponse['title'], "Bad Request");
+    });
+  });
+
+  group('Violations', () {
+    test('should parse from JSON correctly', () {
+      final jsonViolation = {"field": "email", "message": "Email is required"};
+
+      final violation = Violations.fromJson(jsonViolation);
+
+      expect(violation.field, "email");
+      expect(violation.message, "Email is required");
+    });
+
+    test('should convert to JSON correctly', () {
+      final violation =
+          Violations(field: "email", message: "Email is required");
+
+      final jsonViolation = violation.toJson();
+
+      expect(jsonViolation['field'], "email");
+      expect(jsonViolation['message'], "Email is required");
+    });
   });
 }
