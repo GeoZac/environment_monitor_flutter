6e9d9d526b6b20396d1d2bf86fcd8d069c418ffd
George Zacharia <george.zcharia@gmail.com>
2025-03-29 12:45:04 +0530

    test: screens: sensor_readings_test: Remove duplication

--- test/screens/sensor_readings_test.dart
+++ test/screens/sensor_readings_test.dart
@@ -14,6 +14,47 @@ import 'package:http/testing.dart';
 void main() {
   late SensorSystemDTO mockSensorSystem;
 
+  Map<String, dynamic> getMinimalMockReadingData() {
+    return {
+      "isFirst": true,
+      "isLast": true,
+      "hasNext": false,
+      "hasPrevious": false,
+      "data": [
+        {
+          "id": "c4bd4221-369f-4ba8-b3d8-a7c7d6e25c10",
+          "temperature": -8532.269,
+          "humidity": 74.344,
+          "timestamp": "2068-11-12T07:40:41.578527Z",
+          "sensorSystem": {
+            "id": "eacc8c6d-d16b-4a8d-ba83-47f1a2cd4846",
+            "sensorName": "Specific Sensor System",
+            "deleted": false,
+            "sensorStatus": "ACTIVE",
+            "sensorLocation": null,
+            "unconvUser": {
+              "id": "532c476c-0eed-4d46-97f5-e7a6eefaa2d0",
+              "username": "UnconvUser",
+              "email": "unconvuser@email.com"
+            },
+            "readingCount": 0,
+            "latestReading": null,
+          }
+        },
+      ],
+      "totalElements": 1,
+      "pageNumber": 1,
+      "totalPages": 1
+    };
+  }
+
+  MockClient createMockClient({void Function()? onRequest}) {
+    return MockClient((request) async {
+      onRequest?.call();
+      return http.Response(jsonEncode(getMinimalMockReadingData()), 200);
+    });
+  }
+
   setUp(() {
     mockSensorSystem = SensorSystemDTO(
       id: "508baef0-bc82-4481-9af8-83d2e5132100",
@@ -64,42 +105,7 @@ void main() {
 
   testWidgets('Displays sensor readings after data is fetched',
       (WidgetTester tester) async {
-    // Arrange
-    Map<String, dynamic> jsonData = {
-      "isFirst": true,
-      "isLast": true,
-      "hasNext": false,
-      "hasPrevious": false,
-      "data": [
-        {
-          "id": "c4bd4221-369f-4ba8-b3d8-a7c7d6e25c10",
-          "temperature": -8532.269,
-          "humidity": 74.344,
-          "timestamp": "2068-11-12T07:40:41.578527Z",
-          "sensorSystem": {
-            "id": "eacc8c6d-d16b-4a8d-ba83-47f1a2cd4846",
-            "sensorName": "Specific Sensor System",
-            "deleted": false,
-            "sensorStatus": "ACTIVE",
-            "sensorLocation": null,
-            "unconvUser": {
-              "id": "532c476c-0eed-4d46-97f5-e7a6eefaa2d0",
-              "username": "UnconvUser",
-              "email": "unconvuser@email.com"
-            },
-            "readingCount": 0,
-            "latestReading": null,
-          }
-        },
-      ],
-      "totalElements": 1,
-      "pageNumber": 1,
-      "totalPages": 1
-    };
-
-    final mockClient = MockClient((request) async {
-      return http.Response(jsonEncode(jsonData), 200);
-    });
+    final mockClient = createMockClient();
 
     // Act
     await tester.pumpWidget(MaterialApp(
@@ -119,42 +125,8 @@ void main() {
   testWidgets('should call fetchSensorReadings when resumed',
       (WidgetTester tester) async {
     int callCount = 0;
-    Map<String, dynamic> jsonData = {
-      "isFirst": true,
-      "isLast": true,
-      "hasNext": false,
-      "hasPrevious": false,
-      "data": [
-        {
-          "id": "c4bd4221-369f-4ba8-b3d8-a7c7d6e25c10",
-          "temperature": -8532.269,
-          "humidity": 74.344,
-          "timestamp": "2068-11-12T07:40:41.578527Z",
-          "sensorSystem": {
-            "id": "eacc8c6d-d16b-4a8d-ba83-47f1a2cd4846",
-            "sensorName": "Specific Sensor System",
-            "deleted": false,
-            "sensorStatus": "ACTIVE",
-            "sensorLocation": null,
-            "unconvUser": {
-              "id": "532c476c-0eed-4d46-97f5-e7a6eefaa2d0",
-              "username": "UnconvUser",
-              "email": "unconvuser@email.com"
-            },
-            "readingCount": 0,
-            "latestReading": null,
-          }
-        },
-      ],
-      "totalElements": 1,
-      "pageNumber": 1,
-      "totalPages": 1
-    };
 
-    final mockClient = MockClient((request) async {
-      callCount++;
-      return http.Response(jsonEncode(jsonData), 200);
-    });
+    final mockClient = createMockClient(onRequest: () => callCount++);
 
     expect(callCount, 0);
 
