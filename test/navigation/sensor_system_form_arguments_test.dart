5695715c376cb7f6995a662fcbd79930ba2e8915
George Zacharia <george.zcharia@gmail.com>
2025-04-01 14:57:07 +0530

    test: navigation: sensor_system_form_arguments_test: Init

---
+++ test/navigation/sensor_system_form_arguments_test.dart
@@ -0,0 +1,19 @@
+import 'package:environment_monitor/models/user/unconv_user.dart';
+import 'package:environment_monitor/navigation/sensor_system_form_arguments.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('SensorSystemFormArguments', () {
+    test('should store and return the provided UnconvUser', () {
+      final user = UnconvUser(
+        id: "a5bbd1bd-c89b-4219-b0a8-379abe41b879",
+        username: "Test User",
+        email: "test@example.com",
+      );
+
+      final args = SensorSystemFormArguments(user);
+
+      expect(args.unconvUser, equals(user));
+    });
+  });
+}
