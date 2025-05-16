2a801256ed818153345002e23045644b4386fb8a
George Zacharia <george.zcharia@gmail.com>
2025-05-17 03:41:17 +0530

    test: models: user: unconv_user_test: Update for new fields

--- test/models/user/unconv_user_test.dart
+++ test/models/user/unconv_user_test.dart
@@ -1,3 +1,4 @@
+import 'package:environment_monitor/consts/unconv_authority.dart';
 import 'package:environment_monitor/models/user/unconv_user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -7,7 +8,14 @@ void main() {
     final json = {
       "id": "a5bbd1bd-c89b-4219-b0a8-379abe41b879",
       "username": "Test User",
-      "email": "noname@email.com"
+      "email": "noname@email.com",
+      "accountNonExpired": true,
+      "accountNonLocked": true,
+      "credentialsNonExpired": true,
+      "enabled": true,
+      "authorities": [
+        {'authority': 'UNCONV_USER'},
+      ],
     };
 
     // Perform the model mapping
@@ -32,6 +40,13 @@ void main() {
       id: "a5bbd1bd-c89b-4219-b0a8-379abe41b879",
       username: "Test User",
       email: "test@example.com",
+      accountNonExpired: true,
+      accountNonLocked: true,
+      credentialsNonExpired: true,
+      enabled: true,
+      authorities: [
+        UnconvAuthority.unconvUser,
+      ],
     );
 
     final json = instance.toJson();
