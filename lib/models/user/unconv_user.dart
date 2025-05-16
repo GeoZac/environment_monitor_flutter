4b88c6b17afe5ed3c5311874639942e9dccea899
George Zacharia <george.zcharia@gmail.com>
2025-05-16 16:49:28 +0530

    lib: models: user: unconv_user: Add new fields

--- lib/models/user/unconv_user.dart
+++ lib/models/user/unconv_user.dart
@@ -2,11 +2,21 @@ class UnconvUser {
   String id;
   String username;
   String email;
+  bool accountNonExpired;
+  bool accountNonLocked;
+  bool credentialsNonExpired;
+  bool enabled;
+  List<String> authorities;
 
   UnconvUser({
     required this.id,
     required this.username,
     required this.email,
+    required this.accountNonExpired,
+    required this.accountNonLocked,
+    required this.credentialsNonExpired,
+    required this.enabled,
+    required this.authorities,
   });
 
   factory UnconvUser.fromJson(Map<String, dynamic> json) {
@@ -14,6 +24,11 @@ class UnconvUser {
       'id',
       'username',
       'email',
+      'accountNonExpired',
+      'accountNonLocked',
+      'credentialsNonExpired',
+      'enabled',
+      'authorities',
     ];
 
     final bool missingFields =
@@ -26,6 +41,13 @@ class UnconvUser {
       id: json['id'],
       username: json['username'],
       email: json['email'],
+      accountNonExpired: json['accountNonExpired'],
+      accountNonLocked: json['accountNonLocked'],
+      credentialsNonExpired: json['credentialsNonExpired'],
+      enabled: json['enabled'],
+      authorities: (json['authorities'] as List)
+          .map((auth) => auth['authority'].toString())
+          .toList(),
     );
   }
 
@@ -34,6 +56,11 @@ class UnconvUser {
     data['id'] = id;
     data['username'] = username;
     data['email'] = email;
+    data['accountNonExpired'] = accountNonExpired;
+    data['accountNonLocked'] = accountNonLocked;
+    data['credentialsNonExpired'] = credentialsNonExpired;
+    data['enabled'] = enabled;
+    data['authorities'] = authorities.map((a) => {'authority': a}).toList();
     return data;
   }
 }
