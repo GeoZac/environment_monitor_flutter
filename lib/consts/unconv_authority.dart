4d77805c5dd9df5385794722a169026830e5facc
George Zacharia <george.zcharia@gmail.com>
2025-05-18 09:58:34 +0530

    lib: consts: unconv_authority: Extend for all available roles

--- lib/consts/unconv_authority.dart
+++ lib/consts/unconv_authority.dart
@@ -1,11 +1,17 @@
 enum UnconvAuthority {
   unconvUser,
+  unconvAdmin,
+  unconvTenant,
   unconvManager;
 
   static UnconvAuthority fromString(String value) {
     switch (value) {
       case 'UNCONV_USER':
         return UnconvAuthority.unconvUser;
+      case 'UNCONV_ADMIN':
+        return UnconvAuthority.unconvAdmin;
+      case 'UNCONV_TENANT':
+        return UnconvAuthority.unconvTenant;
       case 'UNCONV_MANAGER':
         return UnconvAuthority.unconvManager;
       default:
@@ -17,6 +23,10 @@ enum UnconvAuthority {
     switch (this) {
       case UnconvAuthority.unconvUser:
         return 'UNCONV_USER';
+      case UnconvAuthority.unconvAdmin:
+        return 'UNCONV_ADMIN';
+      case UnconvAuthority.unconvTenant:
+        return 'UNCONV_TENANT';
       case UnconvAuthority.unconvManager:
         return 'UNCONV_MANAGER';
     }
