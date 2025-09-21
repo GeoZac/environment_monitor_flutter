b8193ff60e5b3983539d8efdf89df583556c319b
George Zacharia <george.zcharia@gmail.com>
2025-09-21 07:58:25 +0530

    lib: providers: unconv_user_provider: Implement signup API

--- lib/providers/unconv_user_provider.dart
+++ lib/providers/unconv_user_provider.dart
@@ -1,3 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class UnconvUserProvider with ChangeNotifier {}
+import '../config/globals.dart';
+import '../config/secrets.dart';
+import '../models/user/unconv_user.dart';
+import 'api_provider.dart';
+
+class UnconvUserProvider with ChangeNotifier {
+  final http.Client httpClient;
+
+  UnconvUserProvider(
+    this.httpClient,
+  );
+
+  Future<UnconvUser> createUnconvUser(UnconvUser unconvUser) async {
+    Uri uri = Uri(
+      scheme: Globals.uriScheme,
+      host: Secrets.baseApiUrl,
+      port: Secrets.baseApiPort,
+      path: '/UnconvUser',
+    );
+
+    ApiProvider apiProvider = ApiProvider(
+      httpClient,
+    );
+    final response = await apiProvider.makeHttpPost(
+      uri,
+      null,
+      unconvUser.toJson(),
+    );
+    return UnconvUser.fromJson(
+      response['entity'],
+    );
+  }
+}
