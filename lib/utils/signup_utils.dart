7cf8c367c09f420570bebdb33dec747bb58b9e38
George Zacharia <george.zcharia@gmail.com>
2026-03-27 10:04:30 +0530

    lib: utils: signup_utils: Add a method to validate password
    For signup

---
+++ lib/utils/signup_utils.dart
@@ -0,0 +1,33 @@
+bool isValidPassword(String value) {
+  // Length between 6 and 25
+  if (value.length < 6 || value.length > 25) {
+    return false;
+  }
+
+  // At least one uppercase letter
+  if (!RegExp(r'[A-Z]').hasMatch(value)) {
+    return false;
+  }
+
+  // At least one lowercase letter
+  if (!RegExp(r'[a-z]').hasMatch(value)) {
+    return false;
+  }
+
+  // At least one digit
+  if (!RegExp(r'[0-9]').hasMatch(value)) {
+    return false;
+  }
+
+  // At least one special character
+  if (!RegExp(r'[!@#\$%^&*(),.?":{}|<>_\-\\[\]/+=~`]').hasMatch(value)) {
+    return false;
+  }
+
+  // No whitespace
+  if (RegExp(r'\s').hasMatch(value)) {
+    return false;
+  }
+
+  return true;
+}
