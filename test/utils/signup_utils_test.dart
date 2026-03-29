081212868a5fe78709acf30ea3e59bbe2ff44553
George Zacharia <george.zcharia@gmail.com>
2026-03-29 16:50:59 +0530

    test: utils: signup_utils_test: Add tests for valid username

--- test/utils/signup_utils_test.dart
+++ test/utils/signup_utils_test.dart
@@ -1,1 +1,42 @@
-void main() {}
+import 'package:environment_monitor/utils/signup_utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('isValidUsername', () {
+    test('returns false when value is null', () {
+      expect(isValidUsername(null), false);
+    });
+
+    test('returns false when length is less than 6', () {
+      expect(isValidUsername('Abc1'), false);
+    });
+
+    test('returns false when length is greater than 25', () {
+      expect(isValidUsername('Aabcdefghijklmnopqrstuvwxy'), false);
+    });
+
+    test('returns false when no uppercase letter exists', () {
+      expect(isValidUsername('abcdefg'), false);
+    });
+
+    test('returns false when whitespace is present', () {
+      expect(isValidUsername('Abc def'), false);
+    });
+
+    test('returns true for valid username (min boundary)', () {
+      expect(isValidUsername('Abcdef'), true);
+    });
+
+    test('returns true for valid username (max boundary)', () {
+      expect(isValidUsername('Abcdefghijklmnopqrstuvwxy'), true);
+    });
+
+    test('returns true for typical valid username', () {
+      expect(isValidUsername('UserName123'), true);
+    });
+
+    test('returns true when special characters are present (allowed)', () {
+      expect(isValidUsername('User_Name'), true);
+    });
+  });
+}
